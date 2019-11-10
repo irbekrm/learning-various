033a82ea9119d48ecdf929b6ed1fc8c4d51ab03c
Irbe Krumina <irbekrm@gmail.com>
2019-11-10 13:51:28 +0000

    Adds route table with route to via igw to the bosh public subnet

--- bosh/deploy-bosh/terraform/main/main.tf
+++ bosh/deploy-bosh/terraform/main/main.tf
@@ -81,12 +81,23 @@ resource "aws_internet_gateway" "bosh" {
   }
 }
 
-resource "aws_route" {
-  route_table_id = aws_vpc.bosh_vpc.main_route_table_id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.bosh.id
+resource "aws_route_table" "bosh" {
+  vpc_id = aws_vpc.bosh_vpc_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.bosh.id
+  }
+
+  tags = {
+    Name = "Bosh"
+  }
 }
 
+resource "aws_route_table_association" "bosh" {
+  subnet_id = aws_subnet.bosh_public_subnet.id
+  route_table_id = aws_route_table.bosh.id
+}
 
 
 
