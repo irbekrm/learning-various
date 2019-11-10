23844ae5d26f23c8b0ed8d6e5bb56ffd2b700c05
Irbe Krumina <irbekrm@gmail.com>
2019-11-10 10:36:02 +0000

    Corrects filenames

--- bosh/deploy-bosh/ci/tasks/create_env.sh
+++ bosh/deploy-bosh/ci/tasks/create_env.sh
@@ -7,10 +7,10 @@ jq -r '.bosh_ssh_key' > private_key.pem
 # Interpolate values from Terraform outputs
 
 bosh create-env manifests/bosh.yml \
-  --state=director-state/state.json \
-  --vars-store=director-state/creds.yml \
+  --state=director_state/state.json \
+  --vars-store=director_state/creds.yml \
   -o manifests/cpi.yml \
-  --vars-file=tf-outputs/metadata \
+  --vars-file=tf_infra/metadata \
   -v director-name=bosh \
   -v access_key_id="${AWS_ACCESS_KEY_ID}" \
   -v secret_access_key="${AWS_SECRET_ACCESS_KEY}"
