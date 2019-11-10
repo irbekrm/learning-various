c814e4b18af93e8eb30a424e66b514071b34d97b
Irbe Krumina <irbekrm@gmail.com>
2019-11-10 11:44:08 +0000

    Corrects cloud config path

--- bosh/deploy-bosh/ci/tasks/create_env.sh
+++ bosh/deploy-bosh/ci/tasks/create_env.sh
@@ -9,7 +9,7 @@ jq -r '.bosh_ssh_key' tf_infra/metadata > private_key.pem
 bosh create-env manifests/bosh.yml \
   --state=state_bucket/director_state/state.json \
   --vars-store=state_bucket/director_state/creds.yml \
-  -o manifests/cpi.yml \
+  -o manifests/aws/cpi.yml \
   --vars-file=tf_infra/metadata \
   -v director-name=bosh \
   -v access_key_id="${AWS_ACCESS_KEY_ID}" \
