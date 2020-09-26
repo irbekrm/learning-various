25f8400af96a400be85040ab6cda4983c9cd8651
Irbe Krumina <irbekrm@gmail.com>
2020-09-26 09:17:11 +0100

    Adds a script to deploy Vault

---
+++ vault/scripts/deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -eu
+
+# needs to be ran from learning-various/vault
+
+# Mostly ripped from https://github.com/hashicorp/secrets-store-csi-driver-provider-vault/blob/master/docs/vault-setup.md
+
+kubectl apply -f examples/vault.yaml
+
+# Check if the pod is running
+running=$(kubectl get pod --selector app=vault --output jsonpath="{.items[0].status.phase}")
+while [[ "Running" != "$running" ]]; do
+    echo "Waiting for pod to run"
+    sleep 1
+    running=$(kubectl get pod --selector app=vault --output jsonpath="{.items[0].status.phase}")
+done
+
+# Port forward 
+VAULT_POD=$(kubectl get pod --selector app=vault --output jsonpath="{.items[0].metadata.name}")
+
+cat << EOF
+ To access Vault run
+ kubectl port-forward "${VAULT_POD}" 8200:8200 &
+ export VAULT_ADDR="http://127.0.0.1:8200"
+ export VAULT_TOKEN="root"
+EOF
